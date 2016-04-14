b11bfe3246dad0ecb64f03881f5a51426101b73d
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-04-14 18:52:51 +1000

    Fix incorrect doctest call

--- imod/geometry.py
+++ imod/geometry.py
@@ -39,7 +39,7 @@ def ndline(start, stop, endpoint=False, round=True):
            [0, 0, 1, 0, 0],
            [0, 0, 1, 0, 0],
            [0, 0, 0, 0, 0]])
-    >>> geom.ndline([2, 1, 1], [5, 5, 2.5], endpoint=True)
+    >>> ndline([2, 1, 1], [5, 5, 2.5], endpoint=True)
     [array([2, 3, 4, 4, 5]), array([1, 2, 3, 4, 5]), array([1, 1, 2, 2, 2])]
     """
     start = np.asarray(start)
